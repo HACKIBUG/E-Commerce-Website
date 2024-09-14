6e383256c872c32a697295ffe05c3b907b905941
Pavithra Sakthivel <sakthivelsurekha@gmail.com>
2024-09-14 23:52:10 +0530

    Fourth commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,33 @@ import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
 import Category2 from './components/Category/Category2'
 import Services from './components/Services/Services'
+import Banner from './components/Banner/Banner'
+import Image1 from './assets/hero/headphone.png'
+import Image2 from './assets/category/smartwatch2.png'
+import Products from './components/Products/Products'
+import Blogs from './components/Blogs/Blogs'
+
+
+const BannerData = {
+  discount:"40% Off",
+  title:"Fine Smile",
+  date:"10 Sep to 26 Sep",
+  image:Image1,
+  title2:'Air Solo Bass',
+  title3:'Winter Sale',
+  title4: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, tenetur quisquam. Nihil magni assumenda necessitatibus inventore minima velit, nemo quidem perspiciatis aspernatur, sint deserunt quo aliquam officia saepe esse fugiat.',
+  bgColor:"#f42c37",
+};
+const BannerData2 = {
+  discount:"40% Off",
+  title:"Happy Hours",
+  date:"10 Sep to 26 Sep",
+  image:Image2,
+  title2:'Smart Solo',
+  title3:'Winter Sale',
+  title4: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, tenetur quisquam. Nihil magni assumenda necessitatibus inventore minima velit, nemo quidem perspiciatis aspernatur, sint deserunt quo aliquam officia saepe esse fugiat.',
+  bgColor:"#2dcc61",
+};
 
 
 const App = () => {
@@ -14,6 +41,10 @@ const App = () => {
     <Category></Category>
     <Category2></Category2>
     <Services></Services>
+    <Banner data={BannerData}/>
+    <Products></Products>
+    <Banner data={BannerData2}/>
+    <Blogs></Blogs>
     </div>
   )
 }
---
+++ frontend/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Heading from '../Shared/Heading'
+
+import Img1 from '../../assets/blogs/blog-1.jpg'
+import Img2 from '../../assets/blogs/blog-2.jpg'
+import Img3 from '../../assets/blogs/blog-3.jpg'
+
+
+const BlogData=[
+    {
+        title: 'How to Choose Perfect smartwatch',
+        subtitle: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste vel nam, dolorum alias nemo, necessitatibus tenetur odio repellat, sunt quis illum consequatur veniam unde tempore quo! Exercitationem voluptate quo vero.',
+        published:"Jan 20,2024 by Dilshad",
+        image:Img1
+    },
+    {
+        title: 'How to Choose Perfect smartwatch',
+        subtitle: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste vel nam, dolorum alias nemo, necessitatibus tenetur odio repellat, sunt quis illum consequatur veniam unde tempore quo! Exercitationem voluptate quo vero.',
+        published:"Jan 20,2024 by Dilshad",
+        image:Img2
+    },
+    {
+        title: 'How to Choose Perfect smartwatch',
+        subtitle: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste vel nam, dolorum alias nemo, necessitatibus tenetur odio repellat, sunt quis illum consequatur veniam unde tempore quo! Exercitationem voluptate quo vero.',
+        published:"Jan 20,2024 by Dilshad",
+        image:Img3
+    },
+]
+
+const Blogs = () => {
+  return (
+    <div className='py-12'>
+        <div className='container'>
+      <Heading title='Recent News Feed' subtitle={'Explore Our Blogs'}></Heading>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 gap-y-8 sm:gap-4 md:gap-7'>
+           {
+            BlogData.map((data)=>(
+                <div key={data.title} className='bg-white dark:bg-gray-900'>
+                    <div className='overflow-hidden rounded-2xl mb-2'>
+                        <img src={data.image} alt=""  className='w-full h-[220px] object-cover rounded-2xl hover:scale-105 duration-300'/>
+                    </div>
+                    <div className='space-y-2'>
+                        <p className='text-xs text-gray-500 '>{data.published}</p>
+                        <p className='font-bold line-clamp-1'>{data.title}</p>
+                        <p className='text-sm line-clamp-2 text-gray-700 dark:text-gray-400'>{data.subtitle}</p>
+                    </div>
+                </div>
+            ))
+           }
+      </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Blogs
